23b82c1516a805c1782b01bcf133f88cbd770f8d
Jisu Kim <webmaster@alien.moe>
2020-10-11 06:31:16 +0000

    부팅오류 패치

--- proc.h
+++ proc.h
@@ -49,7 +49,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int switches;
+  int switches;                // Number of context switch
+  int priority;		             // Process Priority
 };
 
 // Process memory is laid out contiguously, low addresses first:
